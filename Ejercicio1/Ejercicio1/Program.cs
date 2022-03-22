4b1a507b36933ca3c4f551423a0bd965f97b983c
unknown <torrez.martina@gmail.com>
2022-03-22 13:59:18 -0300

    first commit

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadNumeros = 5;
+            string numeroConsola;
+            int numero;
+            int maximo=0;
+            int minimo=900;
+            float promedio=0;
+            
+            for(int i = 0; i < cantidadNumeros; i++) 
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numeroConsola = Console.ReadLine();
+               
+                if (int.TryParse(numeroConsola, out numero)) 
+                {
+                    
+                    if (numero > maximo) 
+                    {
+                        maximo = numero;
+                    }
+                    if (numero < minimo ) 
+                    {
+                        minimo = numero;
+                    }
+                    promedio = (promedio + numero);
+                }
+                else 
+                {
+                    Console.WriteLine("Ingrese un numero valido");
+                    i--;
+                }
+
+            }
+            promedio = promedio / cantidadNumeros;
+            Console.WriteLine("El maximo es: {0} El mínimo es: {1} y el promedio es: {2}", maximo, minimo, promedio);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EjercicioDos/EjercicioDos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se debe validar que el número sea mayor que
+//cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+namespace EjercicioDos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroCadena;
+            int numero;
+            int numeroCuadrado;
+            int numeroCubo;
+            Console.Write("Ingrese un numero: ");
+            numeroCadena = Console.ReadLine();
+            int.TryParse(numeroCadena, out numero);
+            while (numero < 0) 
+            {
+                Console.Write("ERROR. ¡Reingresar número!: ");
+                numeroCadena = Console.ReadLine();
+                int.TryParse(numeroCadena, out numero);
+            }
+            numeroCuadrado =(int) Math.Pow(numero, 2);
+            numeroCubo = (int)Math.Pow(numero, 3);
+            Console.WriteLine("{0} al cuadrado es: {1} y al cubo es {2}", numero, numeroCuadrado, numeroCubo);
+            Console.ReadKey();
+        }
+    }
+}
