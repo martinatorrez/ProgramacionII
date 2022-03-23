313133be70c8d690d1fbe24cb99e47915c811397
unknown <torrez.martina@gmail.com>
2022-03-23 01:21:51 -0300

    tercer commit

---
+++ EjercicioI05Tablas/EjercicioI05Tablas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*Crear una aplicación de consola que permita al usuario ingresar un número entero.
+
+Desarrollar un método estático que reciba un número y devuelva la tabla de multiplicación de ese número en formato string.
+
+Se deberá utilizar la clase StringBuilder combinada con strings interpolados para armar el resultado.
+
+Mostrar en la consola el resultado.*/
+namespace EjercicioI05Tablas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
