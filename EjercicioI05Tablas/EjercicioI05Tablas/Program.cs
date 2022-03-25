bdcf87a84b361aab8ad7e5d91abebe26f583d4c5
unknown <torrez.martina@gmail.com>
2022-03-24 21:43:20 -0300

    cuarto commit

--- EjercicioI05Tablas/EjercicioI05Tablas/Program.cs
+++ EjercicioI05Tablas/EjercicioI05Tablas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LogicaNegocio;
 /*Crear una aplicación de consola que permita al usuario ingresar un número entero.
 
 Desarrollar un método estático que reciba un número y devuelva la tabla de multiplicación de ese número en formato string.
@@ -6,13 +7,20 @@ Desarrollar un método estático que reciba un número y devuelva la tabla de mu
 Se deberá utilizar la clase StringBuilder combinada con strings interpolados para armar el resultado.
 
 Mostrar en la consola el resultado.*/
-namespace EjercicioI05Tablas
+namespace Vista
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numeroCadena;
+            int numero;
+            Console.Write("Ingrese un numero: ");
+            numeroCadena = Console.ReadLine();
+            int.TryParse(numeroCadena, out numero);
+
+            Console.WriteLine(Tablas.Multiplicador(numero));
+            Console.ReadKey();
         }
     }
 }
---
+++ EjercicioI05Tablas/LogicaNegocio/Tablas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+namespace LogicaNegocio
+{
+    public class Tablas
+    {
+        public static string Multiplicador(int numero) 
+        {
+            int contador = 1;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat($"Tabla de multiplicar del número {numero}:");
+            stringBuilder.AppendLine("");
+            for (int i = 0; i < 9; i++) 
+            {
+                stringBuilder.AppendFormat($"{numero} x {contador} = {numero * contador}");
+                stringBuilder.AppendLine("");
+                contador++;
+            }
+            
+            return stringBuilder.ToString();
+        }
+    }
+
+}
