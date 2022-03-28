884c1d90837d4710338f592bd00829601f31532b
unknown <torrez.martina@gmail.com>
2022-03-28 18:22:13 -0300

    uno mas

---
+++ Ejercicio6Bisiestos/Ejercicio6Bisiestos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio6Bisiestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string anioInicio;
+            string anioFin;
+            int anioUno;
+            int anioDos;
+            Console.Write("Ingrese un año de inicio: ");
+            anioInicio = Console.ReadLine();
+            int.TryParse(anioInicio, out anioUno);
+            Console.Write("Ingrese un año para finalizar: ");
+            anioFin = Console.ReadLine();
+            int.TryParse(anioFin, out anioDos);
+            for (int i = anioUno; i <= anioDos; i++)
+            {
+                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+                {
+                    Console.WriteLine("Es bisiesto {0}\n", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
