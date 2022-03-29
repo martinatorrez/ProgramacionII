b52ac9be6a2efd5bd84cc755d3600c123d814ba5
unknown <torrez.martina@gmail.com>
2022-03-29 14:58:50 -0300

    prestamo

---
+++ EjercicioI01Prestamo/Entidades/Cuenta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Cuenta
+    {
+        private string titular;
+        private decimal cantidad;
+
+        public Cuenta(string titular, int cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+        public decimal GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string Mostrar()
+        {
+            return $"La razon social del titular es: {this.titular} y el monto actual de dinero en la cuenta es: {this.cantidad}";
+        }
+        public void Ingresar (decimal montoAIngresar) 
+        {
+            if (montoAIngresar > 0) 
+            {
+                cantidad += montoAIngresar;
+            }
+        }
+        public void Retirar(decimal montoARetirar) 
+        {
+            if(montoARetirar > 0) 
+            {
+                this.cantidad -= montoARetirar;
+            }
+        }
+    }
+}
---
+++ EjercicioI01Prestamo/Vista/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Entidades;
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta nuevaCuenta = new Cuenta("Pepito", 7600);
+            Cuenta otraCuenta = new Cuenta("Coso", 2000);
+
+            Console.WriteLine(nuevaCuenta.Mostrar());
+            nuevaCuenta.Ingresar(5000);
+            Console.WriteLine(nuevaCuenta.Mostrar());
+            nuevaCuenta.Retirar(2000);
+            Console.WriteLine(nuevaCuenta.Mostrar());
+            Console.WriteLine("");
+
+            Console.WriteLine(otraCuenta.Mostrar());
+            otraCuenta.Ingresar(1000);
+            Console.WriteLine(otraCuenta.Mostrar());
+            otraCuenta.Retirar(4000);
+            Console.WriteLine(otraCuenta.Mostrar());
+        }
+    }
+}
