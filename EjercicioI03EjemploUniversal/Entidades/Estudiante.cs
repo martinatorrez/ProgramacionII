f4d304ab93d9ddf948669dab9b130cf77b63012f
unknown <torrez.martina@gmail.com>
2022-03-29 16:03:05 -0300

    ejemplo universal

---
+++ EjercicioI03EjemploUniversal/Entidades/Estudiante.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+namespace Entidades
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        static Random random;
+
+        static Estudiante() 
+        {
+            Estudiante.random = new Random();
+        }
+
+        public Estudiante(string nombre, string apellido, string legajo) 
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public void SetNotaPrimerParcial(int value) 
+        {
+            this.notaPrimerParcial = value;
+        }
+
+        public void SetNotaSegundoParcial(int value)
+        {
+            this.notaSegundoParcial = value;
+        }
+
+        private float CalcularPromedio() 
+        {
+            return (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+        }
+
+        public double CalcularNotaFinal() 
+        {
+            if(notaPrimerParcial>3 && notaSegundoParcial > 3) 
+            {
+               return random.Next(6, 10);
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+
+        public string Mostrar() 
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"Nombre y apellido: {nombre} {apellido}. Legajo: {legajo}");
+            sb.AppendLine("");
+            sb.AppendFormat($"Nota primer parcial: {notaPrimerParcial} Nota segundo parcial:{notaSegundoParcial}");
+            sb.AppendLine("");
+            sb.AppendFormat($"Promedio: {CalcularPromedio()}");
+            sb.AppendLine("");
+            if (CalcularNotaFinal() != -1) 
+            {
+                sb.AppendFormat($"Nota final: {CalcularNotaFinal()}");
+                sb.AppendLine("");
+            }
+            else 
+            {
+                sb.AppendLine("Alumno desaprobado");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ EjercicioI03EjemploUniversal/Vista/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Entidades;
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante primerAlumno = new Estudiante("Juan", "Perez", "1234");
+            Estudiante segundoAlumno = new Estudiante("Pepito", "Rodriguez", "1235");
+            Estudiante tercerAlumno = new Estudiante("Maria", "Ramirez", "1236");
+
+            primerAlumno.SetNotaPrimerParcial(4);
+            primerAlumno.SetNotaSegundoParcial(6);
+            
+            segundoAlumno.SetNotaPrimerParcial(4);
+            segundoAlumno.SetNotaSegundoParcial(2);
+
+            tercerAlumno.SetNotaPrimerParcial(8);
+            tercerAlumno.SetNotaSegundoParcial(6);
+
+            Console.WriteLine(primerAlumno.Mostrar());
+            Console.WriteLine(segundoAlumno.Mostrar());
+            Console.WriteLine(tercerAlumno.Mostrar());
+        }
+    }
+}
