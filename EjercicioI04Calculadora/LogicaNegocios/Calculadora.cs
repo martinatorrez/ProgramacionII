9c82c70425a823f46f3ea4cfffa8004e16b1f6dd
unknown <torrez.martina@gmail.com>
2022-03-29 12:32:37 -0300

    calculadora

---
+++ EjercicioI04Calculadora/LogicaNegocios/Calculadora.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LogicaNegocios
+{
+    public class Calculadora
+    {
+        public static float Calcular(float primerOperando, float segundoOperando, char operacion) 
+        {
+            float resultado = 0;
+            switch(operacion)
+            {
+                case '+':
+                    resultado = primerOperando + segundoOperando;
+                    break;
+                case '-':
+                    resultado = primerOperando - segundoOperando;
+                    break;
+                case '*':
+                    resultado = primerOperando * segundoOperando;
+                    break;
+                case '/':
+                    if (Validar(segundoOperando)) 
+                    {
+                        resultado = primerOperando / segundoOperando;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("No es posible dividir por 0");
+                    }
+                   
+                    break;
+            
+            }
+            return resultado;
+        }
+
+        private static bool Validar(float segundoOperando) 
+        {
+            bool retornar = false;
+            if (segundoOperando != 0) 
+            {
+                retornar = true;
+            }
+            return retornar;
+        }
+    }
+}
+/*Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta, multiplicación y división).
+
+Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase):
+
+Calcular (público): Recibirá tres parámetros, el primer operando, el segundo operando y la operación matemática. El método devolverá el resultado de la operación.
+
+Validar (privado): Recibirá como parámetro el segundo operando. Este método se debe utilizar sólo cuando la operación elegida sea la DIVISIÓN. Este método devolverá true si el operando es distinto de cero.
+
+Se le debe pedir al usuario que ingrese dos números y la operación que desea realizar (ingresando el caracter +, -, * o /).
+
+El usuario decidirá cuándo finalizar el programa.*/
---
+++ EjercicioI04Calculadora/Vista/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LogicaNegocios;
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadenaPrimerOperando;
+            string cadenaSegundoOperando;
+            float primerOperando;
+            float segundoOperando;
+            string cadenaOperador;
+            char operador;
+            char continuar='s';
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Calculadora");
+
+                Console.Write("Ingrese un numero: ");
+                cadenaPrimerOperando = Console.ReadLine();
+                float.TryParse(cadenaPrimerOperando, out primerOperando);
+
+                Console.Write("Ingrese otro numero: ");
+                cadenaSegundoOperando = Console.ReadLine();
+                float.TryParse(cadenaSegundoOperando, out segundoOperando);
+
+                Console.Write("Ingrese la operacion que desea realizar (+, -, * o /): ");
+                cadenaOperador = Console.ReadLine();
+                char.TryParse(cadenaOperador, out operador);
+
+                Console.WriteLine($"El resultado de la operacion es : {Calculadora.Calcular(primerOperando,segundoOperando,operador)}");
+
+                Console.Write("Si desea continuar operando presione 's': ");
+                continuar = Console.ReadKey().KeyChar;
+            } while (continuar == 's');
+        }
+    }
+}
+// faltan validaciones
