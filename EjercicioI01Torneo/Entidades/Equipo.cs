7e90a7afb908309e10fa7271abf02635301e4cc1
martinatorrez <torrez.martina@gmail.com>
2022-10-26 17:56:16 -0300

    interfaces

---
+++ EjercicioI01Torneo/Entidades/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Equipo
+    {
+        private string nombre;
+        private DateTime fechaCreacion;
+    }
+}
---
+++ EjercicioI01Torneo/Entidades/Torneo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Entidades
+{
+    public class Torneo<T>where T: Equipo
+    {
+        private string nombre;
+        private List<T> equipos = new List<T>();
+        public static bool operator ==(Torneo<T> torneo, T item) 
+        {
+            bool retorno = false;
+            if(torneo is not null && item is not null) 
+            {
+                if (torneo.equipos.Contains(item)) 
+                {
+                    retorno = true; 
+                }
+            }
+            return retorno;
+        }
+        public static bool operator !=(Torneo<T> torneo, T item) 
+        {
+            return !(torneo == item);
+        }
+        public static bool operator +(Torneo<T> torneo, T item) 
+        {
+            bool retorno=false;
+            if (!(torneo == item)) 
+            {
+                torneo.equipos.Add(item);
+                retorno = true;
+            }
+            return retorno;
+        }
+    }
+}
